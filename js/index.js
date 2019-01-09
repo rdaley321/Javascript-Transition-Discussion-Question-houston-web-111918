d1b9964c214156725776e1fc488ddca26b1f710b
rdaley321 <rdaley321@yahoo.com>
2019-01-09 13:36:13 -0600

    working fade

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
+function fade(element, ms){
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, ms);
 }
+
+function getValue(){
+  const input = document.getElementById('interval')
+  return input.value
+}
+
+const pic = document.getElementById('foreground')
+document.getElementById('save_lincoln').addEventListener('click', function(){fade(pic, getValue())}, false)
